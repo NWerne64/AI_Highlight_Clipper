6ab9097c785c0299db4bcaccad1f7943a8df98a1
noahw <nwerne@stud.hs-heilbronn.de>
2025-03-29 21:04:26 +0100

    Config Server

--- server/config.py
+++ server/config.py
@@ -1,16 +1,16 @@
 class Config:
     twitch_download_app_path = 'twitch_stream_recorder/twitch-recorder.py'
-    google_cloud_storage_bucket_name = 'YOUR-GOOGLE-CLOUD-STORAGE-BUCKET-NAME' # google cloud bucket name
-    webapp_public_ip = '34.122.9.136' # web app public ip
+    google_cloud_storage_bucket_name = 'twitch-highlights-bucket' # google cloud bucket name
+    webapp_public_ip = '35.184.138.82' # web app public ip
     webapp_public_port = '5555' # web app port
 
     #################
     # TWITCH OAUTH
     #################
 
-    nickname = 'YOUR-TWITCH-NAME'
-    secret_id = 'YOUR-TWITCH-SECRET-ID'
-    oauth_token = 'YOUR-TWITCH-OAUTH-TOKEN'
+    nickname = 'Bombenkillerlol'
+    secret_id = 'ikfc4dvetg10iwijx9l2jegtqhaji2'
+    oauth_token = 'falywk58ay2dvd2yj5qw1ybyw0sk9v'
 
 
 
