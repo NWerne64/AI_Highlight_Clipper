25401520d65db2ec688c2e67ddb1b42fb1f24aff
nwerne <nwerne@stud.hs-heilbronn.de>
2025-03-29 18:37:44 +0100

    IP und Twitch Config

--- webapp/viewer/config.py
+++ webapp/viewer/config.py
@@ -4,9 +4,9 @@ class Config:
     # TWITCH CONFIG
     #####################
 
-    client_id = 'TWITCH-CLIENT-ID' # twitch client_id for helix
-    secret_id = 'TWITCH-SECRET-ID' # twitch secret_id for helix
-    server_ip = '34.145.109.225' # ML server public ip
+    client_id = 'l3374um84fruondk4dt8mfua1qg1zm' # twitch client_id for helix
+    secret_id = 'ikfc4dvetg10iwijx9l2jegtqhaji2' # twitch secret_id for helix
+    server_ip = '35.184.138.82' # ML server public ip
     server_port = '5555' # ML server port
 
 
